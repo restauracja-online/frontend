93ad7db5429623c9b2041a6c63a48f98efc5d7a0
Patryk Iwańczyk <patryk.iwanczyk0102@gmail.com>
2021-05-04 20:48:21 +0200

    Added new base components - content - food-categories - restaurant-header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { AppComponent } from './app.component';
 import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { RestaurantHeaderComponent } from './restaurant-header/restaurant-header.component';
+import { ContentComponent } from './content/content.component';
+import { FoodCategoriesComponent } from './food-categories/food-categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderNavComponent
+    HeaderNavComponent,
+    RestaurantHeaderComponent,
+    ContentComponent,
+    FoodCategoriesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/food-categories/food-categories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food-categories',
+  templateUrl: './food-categories.component.html',
+  styleUrls: ['./food-categories.component.css']
+})
+export class FoodCategoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
