aa77c6cfe39bae7f84c8993a431ccda67f01bd4b
Patryk Iwańczyk <patryk.iwanczyk0102@gmail.com>
2021-05-12 19:09:42 +0200

    Add navigations between modals

--- src/app/header-nav/header-nav.component.ts
+++ src/app/header-nav/header-nav.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { faUtensils, faBars } from '@fortawesome/free-solid-svg-icons';
+import {Component, OnInit} from '@angular/core';
+import {faUtensils, faBars} from '@fortawesome/free-solid-svg-icons';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {UserMenuModalComponent} from '../modals/user-menu-modal/user-menu-modal.component';
+import {LoginFormModalComponent} from '../modals/login-form-modal/login-form-modal.component';
+import {ModalEventBusService} from '../modal-event-bus.service';
 
+/*
+* Probably it's good to extract modal logic to separate service or even better use routing to navigate between modals.
+* */
 @Component({
   selector: 'app-header-nav',
   templateUrl: './header-nav.component.html',
@@ -13,13 +18,33 @@ export class HeaderNavComponent implements OnInit {
   faUtensils = faUtensils;
   faBars = faBars;
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal, private modalEventBus: ModalEventBusService) {
+  }
 
   ngOnInit(): void {
+    this.onModalChange();
   }
 
   openUserMenuModal(): void {
     this.modalService.open(UserMenuModalComponent);
+  }
+
+  openLoginFormModal(): void {
+    this.modalService.open(LoginFormModalComponent);
+  }
+
+  onModalChange(): void {
+    this.modalEventBus.onModalRouteChange(target => this.openModalByName(target));
+  }
+
+  openModalByName(targetModal: string): void {
+    this.modalService.dismissAll('Change modal event occurred');
+    switch (targetModal) {
+      case 'login':
+        return this.openLoginFormModal();
+      default:
+        throw new Error('Invalid target modal : ' + targetModal);
+    }
   }
 
 }
--- src/app/modals/user-menu-modal/user-menu-modal.component.ts
+++ src/app/modals/user-menu-modal/user-menu-modal.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {ModalEventBusService} from '../../modal-event-bus.service';
 
 @Component({
   selector: 'app-user-menu-modal',
@@ -8,10 +9,14 @@ import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 })
 export class UserMenuModalComponent implements OnInit {
 
-  constructor(public activeModal: NgbActiveModal) {
+  constructor(public activeModal: NgbActiveModal, public modalEventBus: ModalEventBusService) {
   }
 
   ngOnInit(): void {
+  }
+
+  navigate(targetModal: string): void {
+    this.modalEventBus.emitModalRouteEvent(targetModal);
   }
 
 }
