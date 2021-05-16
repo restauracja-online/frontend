2358fc158cd0bc75a12dde359232d2109aaef29c
Patryk Iwańczyk <patryk.iwanczyk0102@gmail.com>
2021-05-16 18:53:13 +0200

    Remove login button when user is logged

--- src/app/modals/user-menu-modal/user-menu-modal.component.ts
+++ src/app/modals/user-menu-modal/user-menu-modal.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {ModalEventBusService} from '../../modal-event-bus.service';
+import {UserService} from '../../user.service';
 
 @Component({
   selector: 'app-user-menu-modal',
@@ -8,11 +9,13 @@ import {ModalEventBusService} from '../../modal-event-bus.service';
   styleUrls: ['./user-menu-modal.component.css']
 })
 export class UserMenuModalComponent implements OnInit {
+  isUserLogged = false;
 
-  constructor(public activeModal: NgbActiveModal, public modalEventBus: ModalEventBusService) {
+  constructor(public activeModal: NgbActiveModal, public modalEventBus: ModalEventBusService, private userService: UserService) {
   }
 
   ngOnInit(): void {
+    this.isUserLogged = this.userService.isUserLogged();
   }
 
   navigate(targetModal: string): void {
